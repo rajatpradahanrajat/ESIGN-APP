22dcedecad6453d5ac00a445473f652b613f7fb5
rajatpradahanrajat <rajatpradahanrajat@gmail.com>
2024-02-13 12:03:26 +0530

    fgh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Title from "./Components/Title";
 
-function App() {
+const App = () => {
+  const[name,setName]=useState('')
+  const[date,setDate]=useState('DOB')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <h1 className="text-center">Signature App</h1>
+      <Title text={name} />
+      <Title text={date} />
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+        Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras
+        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+        imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+        ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus
+        eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing
+        sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
+        hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec
+        vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit
+        amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris
+        sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget
+        bibendum sodales, augue velit cursus nunc,
+      </p>
+      <div className="d-flex input-boxes">
+        <input type="date" value={date} onChange={(e)=>{setDate(e.target.value)}}/>
+        <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Title = ({text}) => {
+  return (
+<>
+<div className='container'>
+    <h1 className='text-center mt-3'>{ ! text ? 'text':text}</h1>
+
+</div>
+</>
+  )
+}
+
+export default Title
